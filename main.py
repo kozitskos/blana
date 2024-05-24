05f9f4ae2892dc76114e7306f650e9c8a937eb40
kozitskos <kozitskos@gmail.com>
2024-05-24 20:22:29 +0200

    websocket authorization logic draft

--- main.py
+++ main.py
@@ -57,23 +57,84 @@ async def on_shutdown():
     if rabbitmq_connection:
         await rabbitmq_connection.close()
 
+# @sio.event
+# async def connect(sid, environ):
+#     print("Client connected:", sid)
+#     await create_response_queue(sid)
+
 @sio.event
-async def connect(sid, environ):
-    print("Client connected:", sid)
-    await create_response_queue(sid)
+async def connect(sid, environ, auth):
+    db: Session = SessionLocal()
+    try:
+        print('connect ', sid)
+        token = auth.get('token', '').replace('Bearer ', '')
+        print(token)
+        if not token:
+            await sio.disconnect(sid)
+            return
+        
+        user = get_current_user(db, token)
+        if user:
+            await sio.save_session(sid, {'user_id': user.id, 'username': user.username})
+            await create_response_queue(sid)
+            print(f'user {user.id} connected')
+        else:
+            await sio.disconnect(sid)
+    finally:
+        db.close()
 
 @sio.event
 async def disconnect(sid):
     print("Client disconnected:", sid)
     await delete_response_queue(sid)
 
+
+@sio.event
+async def set_note_id(sid, data):
+    print('Received note_id:', data)
+    db: Session = SessionLocal()
+    try:
+        session = await sio.get_session(sid)
+        user_id = session.get('user_id')
+        note_id = data.get('note_id')
+        
+        if not note_id or not user_id:
+            await sio.disconnect(sid)
+            return
+
+        note = crud.get_note_by_id(db, UUID(note_id))
+        if not note or note.owner_id != user_id:
+            await sio.disconnect(sid)
+            return
+
+        session['note_id'] = str(note.id)  # Ensure the note_id is stored as a string
+        await sio.save_session(sid, session)
+        print(f'Note {note.id} associated with user {user_id}')
+    finally:
+        db.close()
+
+# @sio.event
+# async def message(sid, data):
+#     print("Received message:", data)
+#     note_id = UUID(data['note_id'])
+#     audio_data = data['audio']
+#     await send_message(audio_data, sid)
+#     await receive_message(sid, note_id)
+
 @sio.event
 async def message(sid, data):
-    print("Received message:", data)
-    note_id = UUID(data['note_id'])
+    print('Received message:', data)
+    session = await sio.get_session(sid)
+    user_id = session.get('user_id')
+    note_id = session.get('note_id')
+    
+    if not note_id or not user_id:
+        await sio.disconnect(sid)
+        return
+
     audio_data = data['audio']
     await send_message(audio_data, sid)
-    await receive_message(sid, note_id)
+    await receive_message(sid, UUID(note_id))
 
 async def create_response_queue(sid):
     async with rabbitmq_connection.channel() as channel:
